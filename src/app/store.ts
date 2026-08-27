207263a9abb8edb9e1890488ce46dfc43a0bc89a
Edmark Magsalin <emagsalin@cainc.com>
2026-08-27 13:24:37 +0800

    feat: carousel

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,10 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit'
-import weatherReducer from '@/features/weather/weatherSlice'
 import { weatherApi } from '@/services/weatherApi'
 
 export const store = configureStore({
   reducer: {
-    weather: weatherReducer,
     [weatherApi.reducerPath]: weatherApi.reducer
   },
   middleware: (getDefaultMiddleware) => 
--- src/pages/WebApps.tsx
+++ src/pages/WebApps.tsx
@@ -2,8 +2,17 @@ import { Weather } from '@/features/weather/Weather'
 
 export const WebApps = () => {
   return(
-    <div className='page-container grow bg-white/20 backdrop-blur-xl rounded-xl '>
-       <Weather />
+    <div className='page-container bg-white/20 backdrop-blur-xl rounded-xl'>
+       <ul className='carousel scrollbar-none'>
+          <li data-item='1'>
+            <Weather />
+          </li>
+          <li data-item='2'>
+            <div className="flex flex-col justify-center text-center h-full">
+              <small>Currently brewing apps. Stand by for more.</small>
+            </div>
+          </li>
+       </ul>
     </div>
   )
 }
