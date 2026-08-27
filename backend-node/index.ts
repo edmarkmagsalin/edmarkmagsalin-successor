d0b3c63f5cdb413baed4237e8e85c2950f33de3c
Edmark Magsalin <emagsalin@cainc.com>
2026-08-27 15:28:50 +0800

    fix: node backend cors

--- backend-node/index.ts
+++ backend-node/index.ts
@@ -10,7 +10,9 @@ if (!OPENWEATHER_APP_ID) {
   throw new Error('OPENWEATHER_APP_ID is not configured');
 }
 
-app.use(cors());
+app.use(cors({
+  origin: ['https://onrender.com', 'http://localhost:5173'] 
+}));
 app.use(express.json());
 
 app.get('/api/node/current-weather', async (req: Request, res: Response) => {
